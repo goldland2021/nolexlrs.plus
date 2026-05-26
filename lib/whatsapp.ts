4d829e89123e8b84513e17562e1b23dc93898ec4
白昱 <405237967@qq.com>
2026-05-26 09:05:23 +0800

    Update WhatsApp contact number

--- lib/whatsapp.ts
+++ lib/whatsapp.ts
@@ -1,7 +1,7 @@
 export const defaultWhatsAppMessage = "Hello I need airport transfer";
 export const contactEmail = "jpairport.com@gmail.com";
-export const whatsAppDisplayPhone = "+81 80-4789-1812";
-export const whatsAppPhoneNumber = "818047891812";
+export const whatsAppDisplayPhone = "+81 80-9277-6072";
+export const whatsAppPhoneNumber = "818092776072";
 
 export function buildWhatsAppLink(message: string) {
   const encoded = encodeURIComponent(message);
